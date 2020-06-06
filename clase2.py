e8a07aab9a30edf34f8f68665fd9c1d525a8c89a
kthyon <crosalvardo@gmail.com>
2020-06-05 22:42:59 -0500

    segunda clase de python

---
+++ clase2.py
@@ -0,0 +1,61 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 18:31:29 2020
+
+@author: Cristian Alvarado - Seminario python """
+#--------------------------
+#entrada
+#nombre= input("¿cuál es su nombre?: ")
+#edad= input("¿cual es tu edad?")
+#salida
+#print(f"me alegro de conocerte, binvenido {nombre}, veo que tienes {edad} años")
+
+#--------------------------
+#FUNCIONES ARITMETICAS
+#--------------------------
+import math
+
+#print(math.ceil(1.0001))
+
+#print(math.floor(1.9901))
+
+#print(math.fabs(-56))
+
+#print(math.factorial(5))
+
+#print(math.gcd(10,30))
+
+#print(math.trunc(1.0001)) 
+
+#print(math.trunc(3.9999)) 
+
+
+#--------------------------------
+#FUNCIONES TRIGONOMETRICAS
+#-------------------------------
+#print(math.sin(math.pi))#-----0
+#print(math.cos(math.pi))#------1.0
+#print(math.tan(math.pi))#------0
+#print(math.hypot(5,4))#---------6.40
+
+
+
+#--------------------------------
+#FUNCIONES EXPONENCIALES Y LOGARITMICAS
+#--------------------------------
+#print(math.exp(5))
+#print(math.e**5)
+
+#print(math.log(148.4131591025766))
+#print(math.log(148.4131591025766,2))
+
+#print(math.pow(19,6))
+
+
+#print(math.sqrt(125))
+#print(round(2.55326,4))
+
+
+#print(pow(5,3))
+
